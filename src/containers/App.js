c4e62cd8ff7a47149641b195f318bfd31a23619b
Ngoc Ngo Dinh (ngocnd0242) <ngo.dinh.ngoc@sun-asterisk.com>
2019-05-23 15:02:44 +0700

    init

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '../components/Button';
+import NewsItem from '../components/NewsItem'
+import Loading from '../components/Loading'
+let App = () => (
+    <div>
+      <Button />
+      <Loading />
+      <NewsItem />
+    </div>
+);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import createSagaMiddleware from 'redux-saga';
+import {applyMiddleware, createStore} from 'redux';
+import {logger} from 'redux-logger';
+import {Provider} from 'react-redux';
+
+import reducer from './reducers';
+import rootSaga from './sagas';
+
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    reducer,
+    applyMiddleware(sagaMiddleware, logger),
+);
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
